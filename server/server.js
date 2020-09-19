5f09e178df19c4fd8f3a433092452d2fefb7c42c
Nasir Ullah <nasir.developer259@gmail.com>
2020-09-19 21:50:26 +0500

    create todos

--- server/server.js
+++ server/server.js
@@ -13,60 +13,16 @@ const connection = mongoose.connection;
 connection.once('open', function () {
     console.log("MongoDB database connection established successfully");
 })
+app.use(express.static('/public'));
+app.use(express.static(__dirname + '/public', { maxAge: '30 days' }));
+app.use(express.static(__dirname + '/../public'));
+app.use(express.static(__dirname + '/static'));
+
+app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(cors());
 // Put these statements before you define any routes.
 app.use(fileUpload());
 app.use(index);
-
-/* todoRoutes.route('/listAll/:id').get(function (req, res) {
-    TodoModel.findById({ _id: req.params.id }, function (err, record) {
-        if (err)
-            console.log(err);
-        else
-            res.json(record);
-    })
-}) */
-
-/* todoRoutes.route('/add/record').post(function (req, res) {
-    res.json({ requestBody: req.body })
-    console.log(req.body);
-    let todo = new TodoModel(req.body);
-    todo.save()
-        .then(todo => {
-            res.status(200).json({ 'todo': 'todo added successfully' });
-        })
-        .catch(err => {
-            res.status(400).send('adding new todo failed');
-        });
-}); */
-/* todoRoutes.route('/update/:id').post(async function (req, res) {
-    TodoModel.findById({ _id: req.params.id },async function (err, record) {
-        if (err)
-            console.log(err);
-        else {
-            let aggredvalue = await TodoModel.aggregate([
-                {$group : {_id : "$todo_description"}}
-            ]);
-            console.log(aggredvalue);
-            if (record != null) {
-                record.todo_description = req.body.todo_description;
-                record.todo_responsible = req.body.todo_responsible;
-                record.todo_priority = req.body.todo_priority;
-                record.todo_completed = req.body.todo_completed;
-                record.save().then(todo => {
-                    res.json('Todo updated!');
-                })
-                    .catch(err => {
-                        res.status(400).send("Update not possible");
-                    });
-            }
-            else {
-                res.json('no record found');
-            }
-        }
-    })
-}) */
-
 app.listen(PORT, function () {
     console.log(`Server is running on ${PORT}`)
 })
